c2ae6362073d1b1d40c11c122c2dc68162b453bc
Alexander Dolgov <dlgv24@yandex.ru>
2025-04-07 23:21:52 +0300

    fix test | add third argument

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -12,14 +12,14 @@ class GenDiffTest extends TestCase {
     public function testJson12()
     {
         $expected = file_get_contents(FIXTURES_DIR . 'expected1-2.json');
-        $result = genDiff(FIXTURES_DIR . 'file1.json', FIXTURES_DIR . 'file2.json');
+        $result = genDiff(FIXTURES_DIR . 'file1.json', FIXTURES_DIR . 'file2.json', 'json');
         $this->assertEquals($expected, $result);
     }
 
     public function testJson34()
     {
         $expected = file_get_contents(FIXTURES_DIR . 'expected3-4.json');
-        $result = genDiff(FIXTURES_DIR . 'file3.json', FIXTURES_DIR . 'file4.json');
+        $result = genDiff(FIXTURES_DIR . 'file3.json', FIXTURES_DIR . 'file4.json', 'json');
         $this->assertEquals($expected, $result);
     }
 }

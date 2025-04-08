4911adb7810ad8e8f4ce3443437dbb2b4b728eb9
Alexander Dolgov <dlgv24@yandex.ru>
2025-04-08 09:53:06 +0300

    format stylish

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -7,33 +7,40 @@ const INDENT_IN_SPACES = 4;
 function format(mixed $data, int $indent = 0): string
 {
     if (is_object($data)) {
-        $lines = ["{"];
         $vars = get_object_vars($data);
         $keys = array_keys($vars);
 
-        for ($i = 0, $len = count($keys); $i < $len; $i++) {
-            $key = $keys[$i];
-            $value = $vars[$key];
-            $fmt = format($value, $indent + INDENT_IN_SPACES);
-            if (str_starts_with($key, '+ ') || str_starts_with($key, '- ')) {
-                $prefix = str_repeat(' ', $indent + INDENT_IN_SPACES - 2);
-            } else {
-                $prefix = str_repeat(' ', $indent + INDENT_IN_SPACES);
-            }
+        $lines = array_reduce(
+            $keys,
+            function ($carry, $key) use ($indent, $vars) {
+                $value = $vars[$key];
+                $fmt = format($value, $indent + INDENT_IN_SPACES);
+                if (str_starts_with($key, '+ ') || str_starts_with($key, '- ')) {
+                    $prefix = str_repeat(' ', $indent + INDENT_IN_SPACES - 2);
+                } else {
+                    $prefix = str_repeat(' ', $indent + INDENT_IN_SPACES);
+                }
 
-            $lines[] = "{$prefix}{$key}: {$fmt}";
-        }
+                $carry[] = "{$prefix}{$key}: {$fmt}";
+                return $carry;
+            },
+            ['{']
+        );
 
         $lines[] = str_repeat(' ', $indent) . "}";
         $result = implode("\n", $lines);
     } elseif (is_array($data)) {
-        $lines = ["["];
         $prefix = str_repeat(' ', $indent + INDENT_IN_SPACES);
 
-        for ($i = 0, $len = count($data); $i < $len; $i++) {
-            $fmt = format($data[$i], $indent + INDENT_IN_SPACES);
-            $lines[] = "{$prefix}{$fmt}";
-        }
+        $lines = array_reduce(
+            $data,
+            function ($carry, $value) use ($indent, $prefix) {
+                $fmt = format($value, $indent + INDENT_IN_SPACES);
+                $carry[] = "{$prefix}{$fmt}";
+                return $carry;
+            },
+            ['[']
+        );
 
         $lines[] = str_repeat(' ', $indent) . "]";
         $result = implode("\n", $lines);
